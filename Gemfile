e6dab0e99518a7ffb360b29d881673ebcad97b30
Phillip Johnson <pdjohnson84@gmail.com>
2016-12-01 22:23:27 -0800

    setting up for deployment to heroku

--- Gemfile
+++ Gemfile
@@ -1,5 +1,11 @@
+ruby "2.3.0"
+
 source 'https://rubygems.org'
 
 gem 'sinatra', '~>1.4.7'
 gem 'sinatra-contrib'
 gem 'erubis'
+
+group :production do
+  gem "puma"
+end
--- todo.rb
+++ todo.rb
@@ -1,11 +1,15 @@
 require 'sinatra'
-require 'sinatra/reloader'
+require 'sinatra/reloader' if development?
 require 'sinatra/content_for'
 require 'tilt/erubis'
 
 configure do
   enable :sessions
   set :session_secret, 'secret'
+end
+
+helpers do
+
 end
 
 before do
